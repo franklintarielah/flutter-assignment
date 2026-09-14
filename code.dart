fd243cf2535b6748e0c8d05ee8ac3db0a593d1d8
franklintarielah <franklintarielah@gmail.com>
2026-09-14 15:36:09 +0100

    first commit

---
+++ code.dart
@@ -0,0 +1,26 @@
+var adult = "John";
+var youth = "Mike";
+var teenage = "Timothy";
+var age = 50;
+// var age = 22;
+
+
+void main(){
+ 
+  // var adult = "John";
+  // var youth = "Mike";
+  // var teenage = "Timothy";  
+
+
+
+  // if(age >= 40 && adult == "John")
+  // if(age <= 18 && teenage == "Timothy" || youth == "Mike")
+  if(age != 50 && adult =="John")
+  {
+
+      print("you are still growing up");
+  }else{
+    print("you are a Man");
+  }
+
+}
